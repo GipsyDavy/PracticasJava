e7c00c03a995028a41c1fd094226dec811668cdd
Gipsybuho <gipsybuho@gmail.com>
2025-06-24 21:00:32 +0200

    update

---
+++ PracticasJava/src/VariablesOperadores/Practica2.java
@@ -0,0 +1,45 @@
+package VariablesOperadores;
+
+import java.util.Scanner;
+
+public class Practica2 {
+
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		
+		/* Si las notas se quieren introducir con "." en vez de "," hay que cambiar la configuración del Scanner
+		 * 
+		 * var sc = new Scanner(system.in).useLocale(Locale.US);
+		 */
+		
+		System.out.print("Introduzca la primera nota del alumno -> ");
+		double nota1 = sc.nextDouble();
+		
+		System.out.print("Introduzca la segunda nota del alumno -> ");
+		double nota2 = sc.nextDouble();
+		
+		System.out.print("Introduzca la tercera nota del alumno -> ");
+		double nota3 = sc.nextDouble();
+		
+		double notaMedia = (nota1 + nota2 + nota3) / 3;
+		
+		/* para redondear a entero, se puede hacer de dos formas:
+		 * 
+		 * 1. notaMedia = Math.round(notaMedia);
+		 * 2. notaMedia = (int) notaMedia;
+		 */
+		
+		
+		notaMedia = (int) notaMedia;		
+		System.out.println("La nota media del alumno es -> " + notaMedia);
+		
+		String tipoNota = (notaMedia >= 5) ? "El alumno ha APROBADO" : "El alumno ha SUSPENDIDO";
+		System.out.println(tipoNota);
+		
+		sc.close();
+		
+		
+
+	}
+
+}
---
+++ PracticasJava/src/VariablesOperadores/Practica3.java
@@ -0,0 +1,35 @@
+package VariablesOperadores;
+
+import java.util.Scanner;
+
+public class Practica3 {
+
+	public static void main(String[] args) {
+		/* 
+		 * Escribir un programa que calcule el área y el perímetro de un rectángulo y la muestre por consola. La `altura` y la `anchura`
+		 * del mismo la puedes proporcionar como valores iniciales de las variables correspondientes.
+		 */
+		
+		var sc = new Scanner(System.in);
+		
+		System.out.print("Introduce la altura del rectángulo: ");
+		double altura = sc.nextDouble();
+		
+		System.out.print("Introduce el ancho del rectángulo: ");
+		double ancho = sc.nextDouble();
+		
+		
+		double areaRectangulo = altura * ancho;
+		double perimetroRectangulo = 2 * (altura + ancho);
+		
+		areaRectangulo = (int) areaRectangulo;
+		perimetroRectangulo = Math.round(perimetroRectangulo);
+		
+		System.out.println("El área del rectángulo es: " + areaRectangulo);
+		System.out.println("El perímetro del rectángulo es: " + perimetroRectangulo);
+		
+		
+
+	}
+
+}
