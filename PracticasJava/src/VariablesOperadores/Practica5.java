3b6bc4be939b5ec8fbdf6ff78dec8a853d185e50
Gipsybuho <gipsybuho@gmail.com>
2025-06-26 19:28:52 +0200

    update

---
+++ PracticasJava/src/VariablesOperadores/Practica5.java
@@ -0,0 +1,21 @@
+package VariablesOperadores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Practica5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.print("Introduzca la cantida de euros que desea convertir a dolares americanos: ");
+		double euros = sc.nextDouble();
+		double dolares = 1.17;
+		
+		double resultado = euros * dolares;
+		
+		System.out.println(euros + " euros son " + resultado + " dolares");
+
+	}
+
+}
